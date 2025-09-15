a9072e880c5dcb3780c3f43f1fd2b8cf70ede583
MailboxCE <ry_ben@icloud.com>
2025-09-15 16:56:44 -0500

    Add files via upload

---
+++ 1597 3.0 /Programs/RNG.py
@@ -0,0 +1,7 @@
+print("RNG 2023 Mailbox CE. RNG Is Free Code And It Is An Example Code")
+# Program to generate a random number between 0 and 10000000
+
+#importing the random module
+import random
+
+print(random.randint(0,10000000000000000000000000000000000000000000000000000000000000000000))
